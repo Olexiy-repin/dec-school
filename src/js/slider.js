f8580a061acc799d77c74f69c23fb4fdf41ad483
Olexiy repin <repinlyoha@gmail.com>
2018-08-07 01:50:01 +0300

    finish desctop version

--- src/js/slider.js
+++ src/js/slider.js
@@ -57,20 +57,59 @@
   $(".sec-06__slider").owlCarousel({
    items : 1,
    itemsCustom : true,
-   autoplay: true,
-   autoplayTimeout: 3000,
-   animateOut: "fadeOut",
-   animateIn: "fadeIn",
-   mouseDrag: false,
 
    loop: true,
-   nav: false,
-   dot: true,
-
-    autoHeight : false,
+   nav: true,
+   navText: ["<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 477.175 477.175'><path d='M145.188 238.575l215.5-215.5c5.3-5.3 5.3-13.8 0-19.1s-13.8-5.3-19.1 0l-225.1 225.1c-5.3 5.3-5.3 13.8 0 19.1l225.1 225c2.6 2.6 6.1 4 9.5 4s6.9-1.3 9.5-4c5.3-5.3 5.3-13.8 0-19.1l-215.4-215.5z'/></svg>", "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 477.175 477.175'><path d='M360.731 229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1 0s-5.3 13.8 0 19.1l215.5 215.5-215.5 215.5c-5.3 5.3-5.3 13.8 0 19.1 2.6 2.6 6.1 4 9.5 4 3.4 0 6.9-1.3 9.5-4l225.1-225.1c5.3-5.2 5.3-13.8.1-19z'/></svg>"],
+   dots: false,
+   autoHeight : false,
 
     margin : 5
   });
+
+  $(".sec-06__slider").on('changed.owl.carousel', function (event) {
+      let index = event.item.index;
+
+      if (index == 3) {
+        $('.sec-06__slider-nav .active').text('01');
+      } else if (index == 4) {
+        $('.sec-06__slider-nav .active').text('02');
+      } else if (index == 5) {
+        $('.sec-06__slider-nav .active').text('03');
+      } else if (index == 6) {
+        $('.sec-06__slider-nav .active').text('04');
+      } else if (index == 7) {
+        $('.sec-06__slider-nav .active').text('05');
+      } else if (index == 8) {
+        $('.sec-06__slider-nav .active').text('06');
+      } else if (index == 9) {
+        $('.sec-06__slider-nav .active').text('01');
+      }
+    });
+
+    let owlNext = $('.sec-01 .owl-nav .owl-next');
+    let owlPrev = $('.sec-01 .owl-nav .owl-prev');
+    let index = 1;
+
+    owlNext.on('click', function (event) {
+        index++;
+        if (index > 6) {
+          $('.sec-06__slider-nav .active').text((index - 6));
+          index = 1;
+        } else {
+          $('.sec-06__slider-nav .active').text(index);
+        }
+    });
+
+    owlPrev.on('click', function (event) {
+        index--;
+        if (index == 0) {
+          $('.sec-06__slider-nav .active').text((index + 6));
+          index = 6;
+        } else {
+          $('.sec-06__slider-nav .active').text(index);
+        }
+    });
 })();
 
 (() => {
