15b634e78c3ae0a1d8b276dd3a617ce7d2ad7bbf
Olexiy repin <repinlyoha@gmail.com>
2018-08-08 13:19:23 +0300

    add finish changes

--- src/js/slider.js
+++ src/js/slider.js
@@ -121,15 +121,30 @@
     });
 })();
 
+(() => {
+  $(".sec-08__slider").owlCarousel({
+   items : 1,
+   itemsCustom : true,
+
+   loop: true,
+   nav: false,
+   navText: ["<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 477.175 477.175'><path d='M145.188 238.575l215.5-215.5c5.3-5.3 5.3-13.8 0-19.1s-13.8-5.3-19.1 0l-225.1 225.1c-5.3 5.3-5.3 13.8 0 19.1l225.1 225c2.6 2.6 6.1 4 9.5 4s6.9-1.3 9.5-4c5.3-5.3 5.3-13.8 0-19.1l-215.4-215.5z'/></svg>", "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 477.175 477.175'><path d='M360.731 229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1 0s-5.3 13.8 0 19.1l215.5 215.5-215.5 215.5c-5.3 5.3-5.3 13.8 0 19.1 2.6 2.6 6.1 4 9.5 4 3.4 0 6.9-1.3 9.5-4l225.1-225.1c5.3-5.2 5.3-13.8.1-19z'/></svg>"],
+   dots: false,
+   autoplay: true,
+   autoplayTimeout: 5000,
+   animateIn: "fadeIn",
+   animateOut: "fadeOut",
+   autoHeight : false,
+
+    margin : 50
+  });
+})();
+
+
+
 $('.scroll').click(function() {
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
     return false;
-});
-
-$(document).ready(function(){
-  $('.sec-08').slick({
-    // verticalSwiping: true
-  });
 });
